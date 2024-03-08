2aa572dbfb698902b3f93c63a5292ebb4004cd88
BRIJESH <122820661+Brijesh0707@users.noreply.github.com>
2024-03-08 13:52:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import {Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import StudentHome from './pages/StudentHome'
+import AuthForm from './pages/AuthForm'
+import AttendenceTable from './pages/AttendenceTable'
 
-function App() {
+const App = () => {
+  const [auth,setAuth]=useState(false)
+  useEffect(()=>{
+     const token = localStorage.getItem("user_id")
+     if(token){
+      setAuth(true)
+     }else{setAuth(false)}
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+  {auth===true? 
+  <>
+  <Routes>
+  <Route path='/' element={<StudentHome/>}/>
+  {/* <Route path='/admin' element={<Home/>} /> */}
+  <Route path='/admin' element={<Home/>} />
+     <Route path='/attendence/table' element={<AttendenceTable/>} />
+
+  </Routes>
+
+  </>:<>
+  <Routes>
+     <Route path='/' element={<AuthForm/>} />
+     <Route path='/admin' element={<Home/>} />
+     <Route path='/attendence/table' element={<AttendenceTable/>} />
+
+     
+   </Routes>
+
+  </>
+
+  }
+   
+  </>
+  )
 }
 
-export default App;
+export default App
